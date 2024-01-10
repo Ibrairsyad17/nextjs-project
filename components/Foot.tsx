3e4e5f79ba27e06907244ec1cb64b7760eeab482
Ibrahim Irsad <ibrairsyad1712@gmail.com>
2024-01-10 21:44:46 +0700

    change

--- components/Foot.tsx
+++ components/Foot.tsx
@@ -11,8 +11,8 @@ export function Foot() {
     <nav className="sticky bottom-0 l-0 border-gray-200 bg-white w-11/12 sm:w-9/12">
       <div className="relative flex h-16 items-center justify-center">
         <Link href="../../" className="font-medium text-md md:text-lg">
-          Enjoy the <span className="text-red-600">Journey.</span> | &copy;
-          Copyright 2024
+          Enjoy the <span className="text-red-600">Journey.</span> | Copyright
+          2024
         </Link>
       </div>
     </nav>
